c28366e9ad6c60f72ffe24f3443963fb5527bc0f
Calafi <136459515+Calafi@users.noreply.github.com>
2026-03-26 18:10:01 +0100

    stateful hits counter

--- main.go
+++ main.go
@@ -3,16 +3,41 @@ package main
 import (
     "net/http"
     "fmt"
+    "sync/atomic"
 )
 
+type apiConfig struct {
+    fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+    cfg.fileserverHits.Add(1)
+    next.ServeHTTP(w, r)
+    })
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request){
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+        fmt.Fprintf(w, "Hits: %d", hits)
+    }
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request){
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        cfg.fileserverHits.Store(0)
+    }
+
+
 func main(){
+    var apiCfg apiConfig
     mux := http.NewServeMux()
     fileServer := http.FileServer(http.Dir("."))
-    mux.Handle("/app/", http.StripPrefix("/app", fileServer))
-    server := &http.Server{
-        Addr: ":8080",
-	Handler: mux,
-    }
+    handler := http.StripPrefix("/app", fileServer)
+
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 
     mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -20,12 +45,18 @@ func main(){
 	w.Write([]byte("OK"))
     })
 
+    mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+
+    mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+     server := &http.Server{
+        Addr: ":8080",
+        Handler: mux,
+    }
+
     err := server.ListenAndServe()
     if err != nil {
         fmt.Printf("ERROR: %v\n", err)
     }
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-    // your code here
-}
